23324f7f71fa430509cb85e116adb49b23d956e6
Bhupendra Singh <bhupendrasingh.ec18@gmail.com>
2019-06-04 16:19:57 +0530

    remove collaborators in github done

--- Apollo-Graphql/Mutation/gitCollaborators.js
+++ Apollo-Graphql/Mutation/gitCollaborators.js
@@ -82,59 +82,59 @@ userAddInCollaborator.prototype.addCollaboratorGithub = async (root, params, con
 
 
 
-// /*******************************************************************************************************************/
-// /**
-//  * @description : removeCollaboratorGithub APIs for delete collaborators in github repository using apollo-graphql
-//  * @purpose : For gitAuth verification by using CURD operation
-//  * @param {*} root
-//  * @param {*} params
-//  * @param {*} token
-//  */
-// userAddInCollaborator.prototype.removeCollaboratorGithub = async (root, params, context) => {
-//     try {
+/*******************************************************************************************************************/
+/**
+ * @description : removeCollaboratorGithub APIs for delete collaborators in github repository using apollo-graphql
+ * @purpose : For gitAuth verification by using CURD operation
+ * @param {*} root
+ * @param {*} params
+ * @param {*} token
+ */
+userAddInCollaborator.prototype.removeCollaboratorGithub = async (root, params, context) => {
+    try {
 
 
-//         /**
-//         * @param {token}, send token for verify
-//         * @returns {String} message, token verification 
-//         */
-//         var afterVerify = tokenVerify.verification(context.token)
-//         if (!afterVerify > 0) {
-//             return { "message": "token is not verify" }
-//         }
+        /**
+        * @param {token}, send token for verify
+        * @returns {String} message, token verification 
+        */
+        var afterVerify = tokenVerify.verification(context.token)
+        if (!afterVerify > 0) {
+            return { "message": "token is not verify" }
+        }
 
-//         //find token from dataBase
-//         var user = await model.find({ _id: afterVerify.userID })
-//         if (!user) {
-//             return { "message": "user not verified" }
-//         }
+        //find token from dataBase
+        var user = await model.find({ _id: afterVerify.userID })
+        if (!user) {
+            return { "message": "user not verified" }
+        }
 
-//         // Access_token
-//         var access_token = user[0].access_Token
-//         console.log("access_token", access_token)
+        // Access_token
+        var access_token = user[0].access_Token
+        console.log("access_token", access_token)
 
 
-//         /**
-//          * @function (Axios), which is used to handle http request
-//          * @method (DELETE), DELETE data from response when hit the url
-//          * @param {headers}
-//          * @purpose : get response from given url
-//          */
-//         var res = await axios({
-//             method: 'DELETE',
-//             url: `${process.env.GIT_COLAB}${params.ownerName}/${params.repoName}/collaborators/${params.colabUserName}`,
-//             headers: {
-//                 Authorization: `Bearer ${access_token}`
-//             }
-//         })
-//         console.log("\nRepository collaborators Details : ", res);
+        /**
+         * @function (Axios), which is used to handle http request
+         * @method (DELETE), DELETE data from response when hit the url
+         * @param {headers}
+         * @purpose : get response from given url
+         */
+        var res = await axios({
+            method: 'DELETE',
+            url: `${process.env.GIT_COLAB}${params.ownerName}/${params.repoName}/collaborators/${params.colabUserName}`,
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        })
+        console.log("\nRepository collaborators Details : ", res);
 
-//         return { "message": "collborators remove successfully" }
+        return { "message": "collborators remove successfully" }
 
-//     } catch (err) {
-//         return { "message": "collborators alredy removed " }
-//     }
-// }
+    } catch (err) {
+        return { "message": "collborators alredy removed " }
+    }
+}
 
 
 
