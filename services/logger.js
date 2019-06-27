1faddbd6f09be3843e17e77066bfe46f3024d976
Bhupendra Singh <bhupendrasingh.ec18@gmail.com>
2019-06-27 14:40:20 +0530

    logger and coverage updated

--- services/logger.js
+++ services/logger.js
@@ -1,6 +1,26 @@
-//logger
+/********************************************************************************************************************
+ *  @Execution      : default node          : cmd> logger.js
+ *                      
+ * 
+ *  @Purpose        :  A Simple and Universal Logger. ... Winston is a logger for just about everything. 
+ *                     It is designed to be a simple and universal logging library with support for multiple transports.
+ *                     Transport is a storage device for your logs.
+ * 
+ *  @description    : using logger instance we can show or print the value
+ * 
+ *  @overview       : fundoo application  
+ *  @author         : Bhupendra Singh <bhupendrasingh.ec18@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 27-june-2019
+ *
+ *******************************************************************************************************************/
+/**
+ * @requires files
+ */
 var winston = require('winston');
 
+
+//create a winston logger
 var logger = new (winston.createLogger)({
     transports: [
         new (winston.transports.Console)({ json: false, timestamps: true }),
@@ -13,4 +33,7 @@ var logger = new (winston.createLogger)({
     exitOnError: false
 });
 
+/**
+ * @exports logger
+ */
 module.exports = logger;
